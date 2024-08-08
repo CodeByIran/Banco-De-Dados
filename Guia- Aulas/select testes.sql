34c4331d2e0d17ec3e5a915e452340e1fcba5927
naarrI1 <iransantana2624@gmail.com>
2024-08-08 10:26:01 -0300

    Update select testes.sql

--- Guia- Aulas/select testes.sql
+++ Guia- Aulas/select testes.sql
@@ -1,1 +1,4 @@
-SELECT * FROM loja.vendedor WHERE faixa_comissao <> "B"
+SELECT nome, salario_fixo * 1.2 FROM loja.vendedor 
+WHERE faixa_comissao <> "B" AND salario_fixo <= 500;
+
+SELECT * FROM cliente WHERE cidade LIKE "MA%";
