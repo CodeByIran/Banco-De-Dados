104a15efb439103143663dd2a1d8ed497ad6a475
Iran <iransantana2624@gmail.com>
2024-10-31 10:52:45 -0300

    Update inicio-join-exercicioo.sql

--- Guia- Aulas-bimestre3/inicio-join-exercicioo.sql
+++ Guia- Aulas-bimestre3/inicio-join-exercicioo.sql
@@ -9,7 +9,23 @@ FROM curso c INNER JOIN cursodisciplina cd
 ON c.id = FK_disciplina_id
 INNER JOIN disciplina d
 ON cd.FK_disciplina_id = d.id;
+
+-- LISTE TODOS OS ALUNOS MATRICULADOS NAS DISCIPLINAS
+SELECT d.nome AS NomeDisciplinas, a.nome AS AlunosNome
+FROM disciplina d INNER JOIN alunodisciplina ad
+ON d.id =  FK_disciplina_id
+INNER JOIN aluno a 
+ON ad.FK_aluno_mat = d.id;
+
+SELECT a.nome AS AlunoNome, d.nome AS NomeDisciplina
+FROM aluno a INNER JOIN alunodisciplina ad
+ON a.mat = ad.FK_aluno_mat
+INNER JOIN disciplina d 
+ON ad.FK_disciplina_id = d.id;
 # Liste os nomes de todos os professores e as disciplinas que eles ministram
+SELECT p.nome AS professor, d.nome AS disciplina
+FROM professor p INNER JOIN disciplina d
+ON 
 
 # Liste os nomes de todos os alunos e a nota mais alta que eles obtiveram em qualquer disciplina
 
