706f50c55cd803ad112e168a3f2a61256f498b28
Iransx_bjj <iransantana2624@gmail.com>
2025-01-19 20:52:25 -0300

    Update EventoConvidados.js

--- Event-Planner 4°bm/trabalho-finall/models/EventoConvidados.js
+++ Event-Planner 4°bm/trabalho-finall/models/EventoConvidados.js
@@ -4,37 +4,25 @@ const Evento = require('./Evento');
 const Convidados = require('./Convidados');
 
 const EventoConvidados = sequelize.define('EventoConvidados', {
-    FK_Evento_Id_Evento: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Eventos',
-        key: 'Id_Evento',
-      },
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-    },
-    FK_Convidados_Id_Convidado: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Convidados',
-        key: 'Id_Convidado',
-      },
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-    },
-  }, {
-    tableName: 'EventoConvidados',
-    timestamps: true,
-    indexes: [
-      {
-        unique: true,
-        fields: ['FK_Evento_Id_Evento', 'FK_Convidados_Id_Convidado'],
-        name: 'evento_convidados_unique', // Nome curto e descritivo
-      },
-    ],
-  });
+  FK_Evento_Id_Evento: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    references: {
+      model: Evento,
+      key: 'Id_Evento'
+    }
+  },
+  FK_Convidados_Id_Convidado: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    references: {
+      model: Convidados,
+      key: 'Id_Convidado'
+    }
+  }
+}, {
+  timestamps: false
+});
 
 Evento.belongsToMany(Convidados, { through: EventoConvidados, foreignKey: 'FK_Evento_Id_Evento' });
 Convidados.belongsToMany(Evento, { through: EventoConvidados, foreignKey: 'FK_Convidados_Id_Convidado' });
